5e770d3eef4542764a17c1e1e52eb974faec5b08
samirale <samir49715@gmail.com>
2026-01-04 17:10:49 +0100

    maj des pages pour V1

--- PiiScanner.Api/obj/Debug/net8.0/PiiScanner.Api.AssemblyInfo.cs
+++ PiiScanner.Api/obj/Debug/net8.0/PiiScanner.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PiiScanner.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001d5be6a591394d510630e8fea58e18ac8948e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875243151f4cec60918a57943a8f5488f9b2bec5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PiiScanner.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PiiScanner.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
