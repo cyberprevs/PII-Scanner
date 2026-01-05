c4a79ec8715a310d060167923f0f5298a8c3ed3d
samirale <samir49715@gmail.com>
2026-01-05 21:08:01 +0100

    fix

--- PiiScanner.Api/obj/Debug/net8.0/PiiScanner.Api.AssemblyInfo.cs
+++ PiiScanner.Api/obj/Debug/net8.0/PiiScanner.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PiiScanner.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d872bcbdc762ee18ed80431bf2d4eb5029fc9655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+508ab4217309abeef3cd7ed36d54cf628cbfcaeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PiiScanner.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PiiScanner.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
